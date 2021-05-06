b75bef19bb6df91ffb3ec207dfd8c61c4819a97a
Geri Rosenberg <gbrosenberg@wisc.edu>
2021-05-06 14:20:24 -0700

    update headshot

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6889d0a49798c4250f38.js"
+    "url": "webpack-runtime-7433616f007f69a66af0.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8cbb518f6a847863b00419a0e7685de3"
+    "revision": "22b848ac4ebc34146963bc9c0de3c436"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
